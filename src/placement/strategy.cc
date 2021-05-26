2bca06ff6895d2868c2d8a492ed58f06a09ff9d0
mehrdad <mehrdadkhani1@gmail.com>
2021-05-25 17:04:27 -0700

    modularizing the strategy implementation

--- src/placement/strategy.cc
+++ src/placement/strategy.cc
@@ -1,6 +1,51 @@
 #include "strategy.hh"
 #include "op_partitioner.hh"
 
+ExitStatus Strategy::get_hybrid_placement( const uint32_t dp_degree,
+                                           const uint32_t mp_degree,
+                                           const uint32_t global_bs,
+                                           Step &est_steps,
+                                           CG &batch_param_partitioned_graph ){
+  uint16_t local_bs;
+  local_bs = global_bs / dp_degree;
+  input_graph.set_global_batchsize( local_bs );
+//      input_graph.summary( );
+  cout << "===========================================" << endl;
+  cout << "global_bs=" << global_bs << ", "
+       << "dp_degree=" << dp_degree << ", "
+       << "mp_degree=" << mp_degree << ":"
+       << endl;
+  map< Op *, uint32_t > batch_max_splits;
+  for ( auto e : input_graph.adj ) {
+    batch_max_splits[ e.first ] = mp_degree;
+  }
+  OpPartitionerAttribute op_partitioner_batch( batch_quant_step, batch_max_splits, input_graph );
+  CG batch_partitioned_graph;
+  map< Op *, vector< Op * > > parallel_batch_ops_map;
+  op_partitioner_batch.partition( batch_partitioned_graph, parallel_batch_ops_map );
+  assert( input_graph.adj.size( ) == parallel_batch_ops_map.size( ));
+
+  batch_param_partitioned_graph = batch_partitioned_graph;
+  MP pl( interconnect,
+         gpus,
+         interconnect->num_gpus,
+         input_graph,
+         parallel_batch_ops_map,
+         mp_degree /* avail_gpus */,
+         d_max );
+
+  if ( pl.find_placement( batch_param_partitioned_graph ) != ExitStatus::SUCCESS ) {
+    cerr << "OOM on gpus." << endl;
+    batch_param_partitioned_graph.release_ops( ).ok( );
+    return ExitStatus::NOT_AVAILABLE;
+  }
+  pl.add_dp_sync_netops( batch_param_partitioned_graph, parallel_batch_ops_map, mp_degree ).ok( );
+  est_steps = 0;
+//  pl.estimate_iter_time( batch_param_partitioned_graph, est_steps );
+
+  return ExitStatus::SUCCESS;
+}
+
 ExitStatus Strategy::optimize_batchsize( CG &best_graph ) {
   double best_time = std::numeric_limits< double >::max( );
   Step best_itertime;
@@ -8,7 +53,6 @@ ExitStatus Strategy::optimize_batchsize( CG &best_graph ) {
 
   uint32_t dp_degree;
   uint32_t mp_degree;
-  Step batch_quant_step = 30;
 
   auto n_gpus = interconnect->num_gpus;
   for ( auto bs_niter : batchsize_to_niter ) {
@@ -18,37 +62,19 @@ ExitStatus Strategy::optimize_batchsize( CG &best_graph ) {
     for ( int i = 0; i <= log2( n_gpus ); i ++ ) {
       if ( dp_degree > global_bs )
         break;
-      uint16_t local_bs = global_bs / dp_degree;
-      input_graph.set_global_batchsize( local_bs );
+
       mp_degree = interconnect->num_gpus / dp_degree;
-      map< Op *, uint32_t > batch_max_splits;
-      for ( auto e : input_graph.adj ) {
-        batch_max_splits[ e.first ] = mp_degree;
-      }
-      OpPartitionerAttribute op_partitioner_batch( batch_quant_step, batch_max_splits, input_graph );
-      CG batch_partitioned_graph;
-      map< Op *, vector< Op *>> parallel_batch_ops_map;
-      op_partitioner_batch.partition( batch_partitioned_graph, parallel_batch_ops_map );
-      assert( input_graph.adj.size( ) == parallel_batch_ops_map.size( ));
-
+      Step est_steps;
       CG batch_param_partitioned_graph;
-      batch_param_partitioned_graph = batch_partitioned_graph;
-
-      MP pl( interconnect,
-             gpus,
-             interconnect->num_gpus,
-             input_graph,
-             parallel_batch_ops_map,
-             mp_degree /* avail_gpus */,
-             d_max );
-
-      if ( pl.find_placement( batch_param_partitioned_graph ) != ExitStatus::SUCCESS ) {
-        cerr << "OOM on gpus." << endl;
-        batch_param_partitioned_graph.release_ops( ).ok( );
+      ExitStatus is_ok = get_hybrid_placement( dp_degree,
+                            mp_degree,
+                            global_bs,
+                            est_steps,
+                            batch_param_partitioned_graph );
+      if ( is_ok == ExitStatus::NOT_AVAILABLE ){
         continue;
       }
-      Step est_steps;
-      pl.estimate_iter_time( batch_param_partitioned_graph, est_steps );
+//      batch_param_partitioned_graph.summary( );
       double total_time_minutes = double( est_steps ) * double( niter ) * cnfg.step_size_sec / 60.0;
       if ( total_time_minutes < best_time ) {
         best_time = total_time_minutes;
@@ -61,11 +87,8 @@ ExitStatus Strategy::optimize_batchsize( CG &best_graph ) {
       } else {
         batch_param_partitioned_graph.release_ops( ).ok( );
       }
-      cout << "global_bs=" << global_bs << " "
-           << "dp_degree=" << dp_degree << " "
-           << "mp_degree=" << mp_degree << " "
-           << "est_steps=" << est_steps << " "
-           << "total_time_minutes=" << total_time_minutes << " "
+      cout << "est_iter_time=" << est_steps * cnfg.step_size_sec * 1e+3 << "ms, "
+           << "time_to_acc=" << total_time_minutes << " mins."
            << std::endl;
       log << "global_bs=" << global_bs << " "
           << "dp_degree=" << dp_degree << " "
@@ -75,6 +98,8 @@ ExitStatus Strategy::optimize_batchsize( CG &best_graph ) {
           << std::endl;
 //      }
       dp_degree = dp_degree << 1;
+//      if ( dp_degree == 128 )
+//        break;
     }
   }
   std::cout << "best_global_bs=" << best_global_bs << " "
@@ -84,6 +109,7 @@ ExitStatus Strategy::optimize_batchsize( CG &best_graph ) {
             << "best_niter=" << best_niter << " "
             << "best_time=" << best_time << " "
             << std::endl;
+
   return ExitStatus::SUCCESS;
 }
 
--- src/placement/strategy.hh
+++ src/placement/strategy.hh
@@ -13,6 +13,7 @@ class Strategy {
   const CG input_graph;
   BaseInterconnect *interconnect;
   const std::map< uint32_t, uint32_t > batchsize_to_niter;
+  const Step batch_quant_step;
   GPU *gpus;
   const int d_max;
   SimConfig cnfg;
@@ -22,6 +23,7 @@ class Strategy {
   Strategy( const CG &input_graph,
             BaseInterconnect *interconnect,
             std::map< uint32_t, uint32_t > batchsize_to_niter,
+            const Step batch_quant_step,
             GPU *gpus,
             const int d_max,
             const SimConfig cnfg,
@@ -29,11 +31,18 @@ class Strategy {
       : input_graph( input_graph ),
         interconnect( interconnect ),
         batchsize_to_niter( std::move( batchsize_to_niter )),
+        batch_quant_step( batch_quant_step ),
         gpus( gpus ), d_max( d_max ), cnfg( cnfg ), log_dir( log_dir ), log( ) {
     log = std::ofstream( log_dir + "strategy.log", std::ofstream::out );
   }
 
   ExitStatus optimize_batchsize( CG &best_graph );
+
+  ExitStatus get_hybrid_placement( uint32_t dp_degree,
+                                   uint32_t mp_degree,
+                                   uint32_t global_bs,
+                                   Step &est_steps,
+                                   CG &batch_param_partitioned_graph );
 
   ExitStatus raw_placement( CG &best_graph,
                             uint32_t global_bs,
